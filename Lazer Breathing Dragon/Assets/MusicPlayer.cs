74c07936fa57d33b16cb513db6a95ab90d088565
KeeganWoods1 <keeganwoods1@gmail.com>
2020-06-05 18:09:02 -0400

    Terrain for level 1 started, splash screen and theme music added

---
+++ Lazer Breathing Dragon/Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+
+        audioSource = GetComponent<AudioSource>();
+
+        Invoke("LoadNextScene", 3);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
